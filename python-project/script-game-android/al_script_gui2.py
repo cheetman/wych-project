41e71254ac4d164b4616ad6c1589a5ff5bb274cd
cheatman <564778358@qq.com>
2020-06-28 11:40:42 +0800

    update

--- python-project/script-game-android/al_script_gui2.py
+++ python-project/script-game-android/al_script_gui2.py
@@ -15,60 +15,17 @@ import threading
 import scriptModel
 from PIL import Image, ImageFilter
 
-def match_hist_pic(imgHist,templateImgHist,name):
-  match = cv.compareHist(imgHist,templateImgHist,cv.HISTCMP_BHATTACHARYYA)
-  print(name + str(match))
+def showSiftMatchImage(label_img,template,img_target_gray,kps_target,good,matchesMask):
+  draw_params = dict(matchColor=(0,255,0), 
+                            singlePointColor=None,
+                            matchesMask=matchesMask, 
+                            flags=2)
+  result = cv.drawMatches(template.image,template.kps,img_target_gray,kps_target,good,None,**draw_params)
+  cv.imwrite("result.png",result)
+  img_screen = tk.PhotoImage(file="result.png")
+  label_img.config(image=img_screen)  
+  label_img.image= img_screen
 
-def match_hist_pic_and_click_adb(hwnd,name,imgHist,templateImgHist,threshold ,x,y,preImg = None):
-  match = cv.compareHist(imgHist,templateImgHist,cv.HISTCMP_BHATTACHARYYA)
-  if match < threshold:
-    print('直方图匹配-' + name + ' - ' + str(match))
-    os.system( "adb shell input tap "+ str(x) +" " + str(y))
-
-    return True
-  print('直方图未匹配-'+ name + ' - ' + str(match))
-  return False
-
-def match_pic_and_click_adb(hwnd,img_rgb,img_gray,templateImgs,threshold = 0.8,preImg = None):
-  
-
-  if preImg is not None:
-    template_img1 = cv.imdecode(np.fromfile(preImg,dtype=np.uint8),0)
-    h, w = template_img1.shape[:2]
-    res = cv.matchTemplate(img_gray, template_img1, cv.TM_CCOEFF_NORMED)
-    loc = np.where(res >= 0.8) 
-    if len(loc[0]) == 0 :
-      print('未找到目标-' + preImg)
-      return
-
-  for templateImg in templateImgs:
-      # 2.读入模板
-      template_img1 = cv.imdecode(np.fromfile(templateImg,dtype=np.uint8),0)
-      # template_img1 = cv.imread('2.出击图标.jpg', 0)
-      h, w = template_img1.shape[:2]
-
-      # 3.标准相关模板匹配
-      res = cv.matchTemplate(img_gray, template_img1, cv.TM_CCOEFF_NORMED)
-
-      # 4.这边是Python/Numpy的知识，后面解释
-      loc = np.where(res >= threshold)  # 匹配程度大于%80的坐标y,x
-      for pt in zip(*loc[::-1]):  # *号表示可选参数
-          flag = True
-          
-          ptLeft = pt[0]
-          ptTop = pt[1]
-          right_bottom = (pt[0] + w, pt[1] + h)
-          # 调试查看
-          # cv.rectangle(img_rgb, pt, right_bottom, (0, 0, 255), 2)
-
-          print('找到目标-' + templateImg,pt)
-          # dir = 'D:/Program Files/Nox/bin/'
-          os.system( "adb shell input tap "+ str(ptLeft + w//2) +" " + str(ptTop + h//2))
-
-
-          return True
-      print('未找到目标-' + templateImg)
-  return False
 
 #创建sift检测器
 sift=cv.xfeatures2d.SIFT_create()
@@ -79,6 +36,29 @@ templates.append(scriptModel.Template(sift, 3,"3.战术碾压A.jpg"))
 templates.append(scriptModel.Template(sift, 4,"4.获得道具.jpg"))
 templates.append(scriptModel.Template(sift, 5,"6.大获全胜2确定按钮.jpg"))
 templates.append(scriptModel.Template(sift, 6,"5.大获全胜2.jpg"))
+templates.append(scriptModel.Template(sift, 7,"2.出击图标.jpg"))
+templates.append(scriptModel.Template(sift, 8,"1.立刻前往图标.jpg"))
+
+
+template2 = scriptModel.Template(sift, 8,"8.地图条件.jpg")
+templates2 = []
+templates2.append(scriptModel.Template(sift, 1,"8.敌人1.jpg"))
+templates2.append(scriptModel.Template(sift, 1,"8.敌人2.jpg"))
+templates2.append(scriptModel.Template(sift, 1,"8.敌人3.jpg"))
+templates2.append(scriptModel.Template(sift, 1,"8.敌人4.jpg"))
+templates2.append(scriptModel.Template(sift, 1,"8.敌人5.jpg"))
+templates2.append(scriptModel.Template(sift, 1,"8.敌人6.jpg"))
+templates2.append(scriptModel.Template(sift, 1,"8.敌人7.jpg"))
+
+templates3 = []
+templates3.append(scriptModel.Template(sift, 1,"9.海域突进首页.jpg"))
+templates3.append(scriptModel.Template(sift, 1,"9.商船护卫首页.jpg"))
+templates3.append(scriptModel.Template(sift, 1,"9.斩首行动首页.jpg"))
+templates3.append(scriptModel.Template(sift, 1,"9.战术研修首页.jpg"))
+
+templateBoss = scriptModel.Template(sift, 8,"boss.jpg")
+
+templatePosition = scriptModel.Template(sift, 8,"7.定位图标.jpg")
 
 #设置Flannde参数
 FLANN_INDEX_KDTREE=0
@@ -86,54 +66,83 @@ indexParams=dict(algorithm=FLANN_INDEX_KDTREE,trees=5)
 searchParams= dict(checks=50)
 flann=cv.FlannBasedMatcher(indexParams,searchParams)
 
+
+
+def task():
+  if checkBox11Value.get() == 1:
+      # 1.读入截图
+    os.system("adb -s 127.0.0.1:62001 shell screencap -p /sdcard/screen.jpg")
+    os.system("adb -s 127.0.0.1:62001 pull /sdcard/screen.jpg " + os.getcwd())
+    img_target_rgb = cv.imdecode(np.fromfile("screen.jpg",dtype=np.uint8),-1)
+    img_target_gray = cv.cvtColor(img_target_rgb, cv.COLOR_BGR2GRAY)
+    kps_target, features = sift.detectAndCompute(img_target_gray ,None)
+    img_hist = cv.calcHist([img_target_gray],[0],None,[256],[0,256])
+    cv.normalize(img_hist, img_hist,0,255*0.9,cv.NORM_MINMAX)
+    img_rgb_hist = cv.calcHist([img_target_rgb],[0],None,[256],[0,256])
+    cv.normalize(img_rgb_hist, img_rgb_hist,0,255*0.9,cv.NORM_MINMAX)
+
+    # 判断是否在战斗界面
+    success,good,matchesMask,dst = template2.matchSift(flann,img_target_gray,features,kps_target)
+    if success:
+      # 1.在战斗界面
+
+      # 1.1特殊地图
+      if checkBox5Value.get() == 1:
+        # 优先boss
+        if checkBox4Value.get() == 1:
+          success,good,matchesMask,dst = templateBoss.matchSift(flann,img_target_gray,features,kps_target)
+          if success:
+            cv.polylines(img_target_gray,[np.int32(dst)],True,0,2, cv.LINE_AA)
+            os.system( "adb -s 127.0.0.1:62001 shell input tap "+ str((dst[0][0][0] + dst[2][0][0])//2) +" " + str((dst[0][0][1] + dst[2][0][1])//2))
+            showSiftMatchImage(label_img,templateBoss,img_target_gray,kps_target,good,matchesMask)
+            return
+
+        # 普通敌人
+        for template in templates2:
+          time.sleep(0.01)
+          success,good,matchesMask,dst = template.matchSift(flann,img_target_gray,features,kps_target)
+          if success:
+            cv.polylines(img_target_gray,[np.int32(dst)],True,0,2, cv.LINE_AA)
+            os.system( "adb -s 127.0.0.1:62001 shell input tap "+ str((dst[0][0][0] + dst[2][0][0])//2) +" " + str((dst[0][0][1] + dst[2][0][1])//2))
+            showSiftMatchImage(label_img,template,img_target_gray,kps_target,good,matchesMask)
+      else:
+        # 普通敌人
+        for template in templates2:
+          time.sleep(0.01)
+          success,good,matchesMask,dst = template.matchSift(flann,img_target_gray,features,kps_target)
+          if success:
+            cv.polylines(img_target_gray,[np.int32(dst)],True,0,2, cv.LINE_AA)
+            os.system( "adb -s 127.0.0.1:62001 shell input tap "+ str((dst[0][0][0] + dst[2][0][0])//2) +" " + str((dst[0][0][1] + dst[2][0][1])//2))
+            showSiftMatchImage(label_img,template,img_target_gray,kps_target,good,matchesMask)
+            return
+    
+    
+    else:
+      # 2.不在战斗界面
+      if checkBox3Value.get() == 1:
+        for template in templates3:
+          success = template.matchHist(img_rgb_hist)
+          if success:
+            os.system( "adb -s 127.0.0.1:62001 shell input tap "+ str(500) +" " + str(180))
+
+      for template in templates:
+        time.sleep(0.01)
+        success,good,matchesMask,dst = template.matchSift(flann,img_target_gray,features,kps_target)
+        if success:
+          cv.polylines(img_target_gray,[np.int32(dst)],True,0,2, cv.LINE_AA)
+          os.system( "adb -s 127.0.0.1:62001 shell input tap "+ str((dst[0][0][0] + dst[2][0][0])//2) +" " + str((dst[0][0][1] + dst[2][0][1])//2))
+          showSiftMatchImage(label_img,template,img_target_gray,kps_target,good,matchesMask)
+
+
+
+
 def scanGameAdb():
   sleepTime = 4
   while True:
     time.sleep(sleepTime)
     print('是否启动：【%s】' %str(checkBox11Value.get()))
-    if checkBox11Value.get() == 1:
-      # 1.读入截图
-      os.system("adb -s 127.0.0.1:62001 shell screencap -p /sdcard/screen.jpg")
-      os.system("adb -s 127.0.0.1:62001 pull /sdcard/screen.jpg " + os.getcwd())
-      img_target_rgb = cv.imdecode(np.fromfile("screen.jpg",dtype=np.uint8),-1)
-      img_target_gray = cv.cvtColor(img_target_rgb, cv.COLOR_BGR2GRAY)
-      kp2, features = sift.detectAndCompute(img_target_gray ,None)
-
-
-      for template in templates:
-        time.sleep(0.01)
-        success,good = template.matchSift(flann,img_target_gray,features)
-        if success:
-            # 获取关键点的坐标
-            src_pts = np.float32([ template.kps[m.queryIdx].pt for m in good ]).reshape(-1,1,2)
-            dst_pts = np.float32([ kp2[m.trainIdx].pt for m in good ]).reshape(-1,1,2)
-            # 计算变换矩阵和MASK
-            M, mask = cv.findHomography(src_pts, dst_pts, cv.RANSAC, 5.0)
-            matchesMask = mask.ravel().tolist()
-            h,w = template.garyImage.shape
-            # 使用得到的变换矩阵对原图像的四个角进行变换，获得在目标图像上对应的坐标
-            pts = np.float32([ [0,0],[0,h-1],[w-1,h-1],[w-1,0] ]).reshape(-1,1,2)
-            dst = cv.perspectiveTransform(pts,M)
-            cv.polylines(img_target_gray,[np.int32(dst)],True,0,2, cv.LINE_AA)
-            os.system( "adb -s 127.0.0.1:62001 shell input tap "+ str((dst[0][0][0] + dst[2][0][0])//2) +" " + str((dst[0][0][1] + dst[2][0][1])//2))
-        else:
-            matchesMask = None
-        draw_params = dict(matchColor=(0,255,0), 
-                          singlePointColor=None,
-                          matchesMask=matchesMask, 
-                          flags=2)
-        result = cv.drawMatches(template.image,template.kps,img_target_gray,kp2,good,None,**draw_params)
-        
-        if success:
-          cv.imwrite("result.png",result)
-          # im.show()
-          img_screen = tk.PhotoImage(file="result.png")
-          label_img.config(image=img_screen)  
-          label_img.image= img_screen
-          # plt.imshow(result, 'gray')
-          # plt.show()
-
-        sleepTime = 4
+    task()
+    sleepTime = 3
 
 
 window = tk.Tk()            #主窗口
@@ -154,10 +163,10 @@ checkBox6Value = tk.IntVar()
 c1 = tk.Checkbutton(frameLeft, justify=tk.LEFT, text='开始执行', variable=checkBox1Value)
 c11 = tk.Checkbutton(frameLeft,justify=tk.LEFT,  text='开始执行(adb)', variable=checkBox11Value)
 c2 = tk.Checkbutton(frameLeft, justify=tk.LEFT, text='自动寻路', variable=checkBox2Value)
-c3 = tk.Checkbutton(frameLeft, justify=tk.LEFT, text='商船护卫', variable=checkBox3Value)
-c4 = tk.Checkbutton(frameLeft, justify=tk.LEFT, text='海域突进', variable=checkBox4Value)
-c5 = tk.Checkbutton(frameLeft, justify=tk.LEFT, text='斩首行动', variable=checkBox5Value)
-c6 = tk.Checkbutton(frameLeft, justify=tk.LEFT, text='战术研修', variable=checkBox6Value)
+c3 = tk.Checkbutton(frameLeft, justify=tk.LEFT, text='任务界面', variable=checkBox3Value)
+c4 = tk.Checkbutton(frameLeft, justify=tk.LEFT, text='优先BOSS', variable=checkBox4Value)
+c5 = tk.Checkbutton(frameLeft, justify=tk.LEFT, text='特殊地图', variable=checkBox5Value)
+c6 = tk.Checkbutton(frameLeft, justify=tk.LEFT, text='1', variable=checkBox6Value)
 c1.pack()
 c11.pack()
 c2.pack()
--- python-project/script-game-android/scriptModel.py
+++ python-project/script-game-android/scriptModel.py
@@ -1,21 +1,11 @@
 
-
 import numpy as np
 import cv2 as cv
 import matplotlib.pyplot as plt
 
 
-
-
-
-
-
-
-
-
-
 class Template:
-    def __init__(self, sift, name, filename, siftRatio=0.2):
+    def __init__(self, sift, name, filename, siftRatio=0.2,histThreshold = 0.02):
         self.name = name
         self.image = cv.imdecode(np.fromfile(filename,dtype=np.uint8),-1)
         self.garyImage = cv.cvtColor(self.image, cv.COLOR_BGR2GRAY)
@@ -25,17 +15,42 @@ class Template:
         if len(self.kps) == 0 or self.features is None:
             raise Exception("无法检测到特征点：{0}".format(filename))
         self.siftRatio = siftRatio
+        self.garyImageHist = cv.calcHist([self.garyImage],[0],None,[256],[0,256])
+        cv.normalize(self.garyImageHist, self.garyImageHist,0,255*0.9,cv.NORM_MINMAX)
+        self.imageHist = cv.calcHist([self.image],[0],None,[256],[0,256])
+        cv.normalize(self.imageHist, self.imageHist,0,255*0.9,cv.NORM_MINMAX)
+        self.histThreshold = histThreshold
 
-    def matchSift(self, flann,targetImg,targetFeatures):
+    def matchSift(self, flann,targetImg,targetFeatures,kps):
         matches = flann.knnMatch(self.features, targetFeatures, 2)
         good = []
         for m, n in matches:
             if m.distance < n.distance * 0.66:
                 good.append(m)
         if len(good) > len(self.kps) * self.siftRatio:
-            return True,good
+            # 获取关键点的坐标
+            src_pts = np.float32([ self.kps[m.queryIdx].pt for m in good ]).reshape(-1,1,2)
+            dst_pts = np.float32([ kps[m.trainIdx].pt for m in good ]).reshape(-1,1,2)
+            # 计算变换矩阵和MASK
+            M, mask = cv.findHomography(src_pts, dst_pts, cv.RANSAC, 5.0)
+            if M is None:
+                print( "M is None - " + str(self.name) )
+                return False,good,None,None
+            matchesMask = mask.ravel().tolist()
+            h,w = self.garyImage.shape
+            # 使用得到的变换矩阵对原图像的四个角进行变换，获得在目标图像上对应的坐标
+            pts = np.float32([ [0,0],[0,h-1],[w-1,h-1],[w-1,0] ]).reshape(-1,1,2)
+            dst = cv.perspectiveTransform(pts,M)
+            return True,good,matchesMask,dst
         else:
-            print( "Not enough matches are found - " + self.name )
-            return False,good
+            print( "Not enough matches are found - " + str(self.name) )
+            return False,good,None,None
 
-        
+    def matchHist(self, imgHist):
+        match = cv.compareHist(imgHist,self.imageHist,cv.HISTCMP_BHATTACHARYYA)
+        if match < self.histThreshold:
+            print('直方图匹配-' + str(self.name) + ' - ' + str(match))
+            return True
+        else:
+            print('直方图未匹配-'+ str(self.name) + ' - ' + str(match))
+            return False

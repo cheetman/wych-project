c9145acf5a0ca52e443385b82422eb83d85f8376
cheatman <564778358@qq.com>
2020-07-17 10:07:49 +0800

    update

--- c-project/formula/HelloWorld.cpp
+++ c-project/formula/HelloWorld.cpp
@@ -1,8 +1,23 @@
 #include <stdio.h>
 #include <windows.h>
+#include <iostream>
+#include<opencv2/opencv.hpp>
+#include<opencv2/highgui.hpp>
+using namespace std;
+using namespace cv;
 int main()
 {
+
+
+
+    
     printf("Hello World\n");
+    cout << "Hello World" << endl;
+
+    Mat img=imread("C:\\Users\\cheet\\Desktop\\QQ20200625190228.jpg");
+    cv::imshow("image",img);
+    cv::waitKey();
+
     system("pause");
     return 0;
 }

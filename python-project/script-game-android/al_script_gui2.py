974649a64fa85bbedafa6e04255d63cedce6318a
hpx <774022063@qq.com>
2020-10-26 19:37:14 +0800

    update

--- python-project/script-game-android/al_script_gui2.py
+++ python-project/script-game-android/al_script_gui2.py
@@ -42,13 +42,19 @@ templates.append(scriptModel.Template(sift, 8,"1.立刻前往图标.jpg"))
 
 template2 = scriptModel.Template(sift, 8,"8.地图条件.jpg")
 templates2 = []
-templates2.append(scriptModel.Template(sift, 1,"8.敌人1.jpg"))
-templates2.append(scriptModel.Template(sift, 1,"8.敌人2.jpg"))
-templates2.append(scriptModel.Template(sift, 1,"8.敌人3.jpg"))
-templates2.append(scriptModel.Template(sift, 1,"8.敌人4.jpg"))
-templates2.append(scriptModel.Template(sift, 1,"8.敌人5.jpg"))
-templates2.append(scriptModel.Template(sift, 1,"8.敌人6.jpg"))
-templates2.append(scriptModel.Template(sift, 1,"8.敌人7.jpg"))
+# templates2.append(scriptModel.Template(sift, 1,"8.敌人1.jpg"))
+# templates2.append(scriptModel.Template(sift, 1,"8.敌人2.jpg"))
+# templates2.append(scriptModel.Template(sift, 1,"8.敌人3.jpg"))
+# templates2.append(scriptModel.Template(sift, 1,"8.敌人4.jpg"))
+# templates2.append(scriptModel.Template(sift, 1,"8.敌人5.jpg"))
+# templates2.append(scriptModel.Template(sift, 1,"8.敌人6.jpg"))
+# templates2.append(scriptModel.Template(sift, 1,"8.敌人7.jpg"))
+
+files = os.listdir('.\8.敌人目录')
+for file in files:
+  path = os.path.join('.\8.敌人目录', file)
+  templates2.append(scriptModel.Template(sift, 1,path))
+  
 
 templates3 = []
 templates3.append(scriptModel.Template(sift, 1,"9.海域突进首页.jpg"))
@@ -109,6 +115,16 @@ def task():
             os.system( "adb -s 127.0.0.1:62001 shell input tap "+ str((dst[0][0][0] + dst[2][0][0])//2) +" " + str((dst[0][0][1] + dst[2][0][1])//2))
             showSiftMatchImage(label_img,template,img_target_gray,kps_target,good,matchesMask)
       else:
+
+        # 优先boss
+        if checkBox4Value.get() == 1:
+          success,good,matchesMask,dst = templateBoss.matchSift(flann,img_target_gray,features,kps_target)
+          if success:
+            cv.polylines(img_target_gray,[np.int32(dst)],True,0,2, cv.LINE_AA)
+            os.system( "adb -s 127.0.0.1:62001 shell input tap "+ str((dst[0][0][0] + dst[2][0][0])//2) +" " + str((dst[0][0][1] + dst[2][0][1])//2))
+            showSiftMatchImage(label_img,templateBoss,img_target_gray,kps_target,good,matchesMask)
+            return
+            
         # 普通敌人
         for template in templates2:
           time.sleep(0.01)

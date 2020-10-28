2d341de89c49ad23eeb765dc69f4bb46953b9993
hpx <774022063@qq.com>
2020-10-28 15:24:22 +0800

    追加dc

--- python-project/script-game-android/al_script_gui2.py
+++ python-project/script-game-android/al_script_gui2.py
@@ -11,6 +11,7 @@ import time
 import argparse
 import glob
 import tkinter as tk
+import tkinter.font as tkFont
 import threading
 import scriptModel
 import subprocess
@@ -27,45 +28,46 @@ def showSiftMatchImage(label_img,template,img_target_gray,kps_target,good,matche
   label_img.config(image=img_screen)  
   label_img.image= img_screen
 
-
 #创建sift检测器
 sift=cv.xfeatures2d.SIFT_create()
 
   
-
-template2 = scriptModel.Template(sift, 8,"8.地图条件.jpg")
-
-
-
 templates = []
-templatesDir = '.\\1.图标点击'
+templatesDir = './1.图标点击'
 files = os.listdir(templatesDir)
 for file in files:
   path = os.path.join(templatesDir, file)
   templates.append(scriptModel.Template(sift, 1,path))
-
 templates2 = []
-templatesDir = '.\\8.敌人目录'
+templatesDir = './8.敌人目录'
 files = os.listdir(templatesDir)
 for file in files:
   path = os.path.join(templatesDir, file)
   templates2.append(scriptModel.Template(sift, 1,path))
-  
-
+templatesBoss = []
+templatesDir = './2.BOSS目录'
+files = os.listdir(templatesDir)
+for file in files:
+  path = os.path.join(templatesDir, file)
+  templatesBoss.append(scriptModel.Template(sift, 8,path))
+template2 = scriptModel.Template(sift, 8,"8.地图条件.jpg")
 templates3 = []
 templates3.append(scriptModel.Template(sift, 1,"9.海域突进首页.jpg"))
 templates3.append(scriptModel.Template(sift, 1,"9.商船护卫首页.jpg"))
 templates3.append(scriptModel.Template(sift, 1,"9.斩首行动首页.jpg"))
-
 templates3_2 = []
 templates3_2.append(scriptModel.Template(sift, 1,"9.战术研修首页.jpg"))
-
 templates4 = []
 templates4.append(scriptModel.Template(sift, 1,"9.特别演习-埃塞克斯.jpg"))
-
-templateBoss = scriptModel.Template(sift, 8,"boss.jpg")
-
 templatePosition = scriptModel.Template(sift, 8,"7.定位图标.jpg")
+
+dcTemplates=[]
+templatesDir = './DC.1.图标点击'
+files = os.listdir(templatesDir)
+for file in files:
+  path = os.path.join(templatesDir, file)
+  dcTemplates.append(scriptModel.Template(sift, 1,path))
+
 
 #设置Flannde参数
 FLANN_INDEX_KDTREE=0
@@ -74,11 +76,29 @@ searchParams= dict(checks=50)
 flann=cv.FlannBasedMatcher(indexParams,searchParams)
 
 
+def taskDc():
+  screenFileName = "screenDC.jpg"
+  if checkBox201Value.get() == 1:
+    subprocess.getstatusoutput([entry1Value.get(),"-s",entry22Value.get(),"shell", "screencap", "-p", "/sdcard/"+screenFileName])
+    subprocess.getstatusoutput([entry1Value.get(),"-s",entry22Value.get(),"pull", "/sdcard/"+screenFileName, os.getcwd()])
+    img_target_rgb = cv.imdecode(np.fromfile(screenFileName,dtype=np.uint8),-1)
+    img_target_gray = cv.cvtColor(img_target_rgb, cv.COLOR_BGR2GRAY)
+    kps_target, features = sift.detectAndCompute(img_target_gray ,None)
+    img_hist = cv.calcHist([img_target_gray],[0],None,[256],[0,256])
+    cv.normalize(img_hist, img_hist,0,255*0.9,cv.NORM_MINMAX)
+    img_rgb_hist = cv.calcHist([img_target_rgb],[0],None,[256],[0,256])
+    cv.normalize(img_rgb_hist, img_rgb_hist,0,255*0.9,cv.NORM_MINMAX)
+
+    for template in dcTemplates: # 2.3 看见就点图标(例如任务成功)
+      time.sleep(0.01)
+      success,good,matchesMask,dst = template.matchSift(flann,img_target_gray,features,kps_target)
+      if success:
+        cv.polylines(img_target_gray,[np.int32(dst)],True,0,2, cv.LINE_AA)
+        subprocess.getstatusoutput([entry1Value.get(),"-s",entry22Value.get(),"shell","input","tap",str((dst[0][0][0] + dst[2][0][0])//2) , str((dst[0][0][1] + dst[2][0][1])//2)])
+        showSiftMatchImage(label_img,template,img_target_gray,kps_target,good,matchesMask)
 
 def task():
   if checkBox11Value.get() == 1:
-      # 1.读入截图
-
     subprocess.getstatusoutput([entry1Value.get(),"-s",entry4Value.get(),"shell", "screencap", "-p", "/sdcard/screen.jpg"])
     subprocess.getstatusoutput([entry1Value.get(),"-s",entry4Value.get(),"pull", "/sdcard/screen.jpg", os.getcwd()])
     # os.system(entry1Value.get() + " shell screencap -p /sdcard/screen.jpg")
@@ -91,21 +111,20 @@ def task():
     img_rgb_hist = cv.calcHist([img_target_rgb],[0],None,[256],[0,256])
     cv.normalize(img_rgb_hist, img_rgb_hist,0,255*0.9,cv.NORM_MINMAX)
 
-    
     success,good,matchesMask,dst = template2.matchSift(flann,img_target_gray,features,kps_target)
-    if success: # 判断是否在战斗界面
+    if success: # 判断是否在作战地图
     # 1.在战斗界面
       if checkBox5Value.get() == 1:
       # 1.1 特殊地图
-
-        
         if checkBox4Value.get() == 1: # 判断是否优先Boss
-          success,good,matchesMask,dst = templateBoss.matchSift(flann,img_target_gray,features,kps_target)
-          if success:
-            cv.polylines(img_target_gray,[np.int32(dst)],True,0,2, cv.LINE_AA)
-            subprocess.getstatusoutput([entry1Value.get(),"-s",entry4Value.get(),"shell","input","tap",str((dst[0][0][0] + dst[2][0][0])//2) ,str((dst[0][0][1] + dst[2][0][1])//2)])
-            showSiftMatchImage(label_img,templateBoss,img_target_gray,kps_target,good,matchesMask)
-            return
+          for templateBoss in templatesBoss:
+            time.sleep(0.01)
+            success,good,matchesMask,dst = templateBoss.matchSift(flann,img_target_gray,features,kps_target)
+            if success:
+              cv.polylines(img_target_gray,[np.int32(dst)],True,0,2, cv.LINE_AA)
+              subprocess.getstatusoutput([entry1Value.get(),"-s",entry4Value.get(),"shell","input","tap",str((dst[0][0][0] + dst[2][0][0])//2) ,str((dst[0][0][1] + dst[2][0][1])//2)])
+              showSiftMatchImage(label_img,templateBoss,img_target_gray,kps_target,good,matchesMask)
+              return
 
         for template in templates2:
           time.sleep(0.01)
@@ -118,12 +137,14 @@ def task():
       # 1.2 普通地图
 
         if checkBox4Value.get() == 1: # 判断是否优先Boss
-          success,good,matchesMask,dst = templateBoss.matchSift(flann,img_target_gray,features,kps_target)
-          if success:
-            cv.polylines(img_target_gray,[np.int32(dst)],True,0,2, cv.LINE_AA)
-            subprocess.getstatusoutput([entry1Value.get(),"-s",entry4Value.get(),"shell","input","tap", str((dst[0][0][0] + dst[2][0][0])//2) , str((dst[0][0][1] + dst[2][0][1])//2)])
-            showSiftMatchImage(label_img,templateBoss,img_target_gray,kps_target,good,matchesMask)
-            return
+          for templateBoss in templatesBoss:
+            time.sleep(0.01)
+            success,good,matchesMask,dst = templateBoss.matchSift(flann,img_target_gray,features,kps_target)
+            if success:
+              cv.polylines(img_target_gray,[np.int32(dst)],True,0,2, cv.LINE_AA)
+              subprocess.getstatusoutput([entry1Value.get(),"-s",entry4Value.get(),"shell","input","tap", str((dst[0][0][0] + dst[2][0][0])//2) , str((dst[0][0][1] + dst[2][0][1])//2)])
+              showSiftMatchImage(label_img,templateBoss,img_target_gray,kps_target,good,matchesMask)
+              return
 
         for template in templates2:
           time.sleep(0.01)
@@ -183,27 +204,27 @@ def scanGameAdb():
     time.sleep(sleepTime)
     print('是否启动：【%s】' %str(checkBox11Value.get()))
     task()
+    print('是否启动DC：【%s】' %str(checkBox11Value.get()))
+    taskDc()
     sleepTime = 3
 
 
 
-window = tk.Tk()            #主窗口
-window.title('碧蓝航线脚本(夜神模拟器)')   #窗口标题
-window.geometry('700x400')  #窗口尺寸
-
+window = tk.Tk()           
+window.title('碧蓝航线&DC脚本(夜神模拟器)')   
+window.geometry('700x400')  
 menubar = tk.Menu(window)
 filemenu = tk.Menu(menubar, tearoff=0)
 menubar.add_cascade(label='File', menu=filemenu)
 filemenu.add_command(label='Exit', command=window.quit)
 window.config(menu=menubar)
-
 frameLeft = tk.Frame(window)
 frameRight = tk.Frame(window)
 frameBottom = tk.Frame(window)
-
 frameLeft.pack(side=tk.LEFT, fill='y', expand='no')
 frameRight.pack(side=tk.TOP, fill='x')
 frameBottom.pack(side=tk.RIGHT, expand='yes', fill='both')
+boldFont = tkFont.Font(family='Fixdsys', size=10, weight=tkFont.BOLD)
 
 checkBox1Value = tk.IntVar()
 checkBox11Value = tk.IntVar()
@@ -214,47 +235,44 @@ checkBox5Value = tk.IntVar()
 checkBox6Value = tk.IntVar()
 checkBox7Value = tk.IntVar()
 checkBox8Value = tk.IntVar()
+checkBox201Value = tk.IntVar()
 entry1Value = tk.StringVar(value='C:/Program Files (x86)/Nox/bin/adb')
 entry4Value = tk.StringVar(value='127.0.0.1:62001')
 entry2Value = tk.StringVar(value='500,180')
 entry3Value = tk.StringVar(value='500,180')
+entry21Value = tk.StringVar(value='C:/leidian/LDPlayer64/adb')
+entry22Value = tk.StringVar(value='emulator-5554')
 
-c1 = tk.Checkbutton(frameLeft, justify=tk.LEFT, text='开始执行', variable=checkBox1Value).pack()
-c11 = tk.Checkbutton(frameLeft,justify=tk.LEFT,  text='开始执行(adb)', variable=checkBox11Value).pack()
-c2 = tk.Checkbutton(frameLeft, justify=tk.LEFT, text='自动寻路', variable=checkBox2Value).pack()
-c3 = tk.Checkbutton(frameLeft, justify=tk.LEFT, text='任务界面', variable=checkBox3Value).pack()
-c4 = tk.Checkbutton(frameLeft, justify=tk.LEFT, text='优先BOSS', variable=checkBox4Value).pack()
-c5 = tk.Checkbutton(frameLeft, justify=tk.LEFT, text='特殊地图', variable=checkBox5Value).pack()
-c6 = tk.Checkbutton(frameLeft, justify=tk.LEFT, text='特演-埃塞克斯', variable=checkBox6Value).pack()
-c7 = tk.Checkbutton(frameLeft, justify=tk.LEFT, text='特演-埃塞克斯(普通)', variable=checkBox7Value).pack()
-c8 = tk.Checkbutton(frameLeft, justify=tk.LEFT, text='特演-埃塞克斯(简单)', variable=checkBox8Value).pack()
-
+tk.Label(frameLeft,text="碧蓝航线",font=boldFont).pack()
+tk.Checkbutton(frameLeft, justify=tk.LEFT, text='开始执行', variable=checkBox1Value).pack()
+tk.Checkbutton(frameLeft,justify=tk.LEFT,  text='开始执行(adb)', variable=checkBox11Value).pack()
+tk.Checkbutton(frameLeft, justify=tk.LEFT, text='自动寻路', variable=checkBox2Value).pack()
+tk.Checkbutton(frameLeft, justify=tk.LEFT, text='任务界面', variable=checkBox3Value).pack()
+tk.Checkbutton(frameLeft, justify=tk.LEFT, text='优先BOSS', variable=checkBox4Value).pack()
+tk.Checkbutton(frameLeft, justify=tk.LEFT, text='特殊地图', variable=checkBox5Value).pack()
+tk.Checkbutton(frameLeft, justify=tk.LEFT, text='特演-埃塞克斯', variable=checkBox6Value).pack()
+tk.Checkbutton(frameLeft, justify=tk.LEFT, text='特演-埃塞克斯(普通)', variable=checkBox7Value).pack()
+tk.Checkbutton(frameLeft, justify=tk.LEFT, text='特演-埃塞克斯(简单)', variable=checkBox8Value).pack()
+tk.Label(frameLeft,text="DC",font=boldFont).pack()
+tk.Checkbutton(frameLeft,justify=tk.LEFT,  text='开始执行(adb)', variable=checkBox201Value).pack()
 
 tk.Label(frameBottom,text="命令地址:").grid(row=0,column=0)
 tk.Entry(frameBottom,textvariable = entry1Value).grid(row=0,column=1,columnspan=2,sticky=tk.EW)
 tk.Entry(frameBottom,textvariable = entry4Value).grid(row=0,column=3)
-
+tk.Label(frameBottom,text="DC命令地址:").grid(row=2,column=0)
+tk.Entry(frameBottom,textvariable = entry1Value).grid(row=2,column=1,columnspan=2,sticky=tk.EW)
+tk.Entry(frameBottom,textvariable = entry22Value).grid(row=2,column=3)
 tk.Label(frameBottom,text="普通任务坐标:").grid(row=1,column=0)
 tk.Label(frameBottom,text="战术研修坐标:").grid(row=1,column=2)
-
 tk.Entry(frameBottom,textvariable = entry2Value).grid(row=1,column=1)
 tk.Entry(frameBottom,textvariable = entry3Value).grid(row=1,column=3)
 
-# e1.grid(row=0, column=0, padx=10, pady=10, ipadx=10, ipady=10)
-# e2.grid(row=0, column=1, padx=10, pady=10, ipadx=10, ipady=10)
-# e3.grid(row=1, column=0, padx=10, pady=10, ipadx=10, ipady=10)
 
 
 img_screen = tk.PhotoImage(file="screen.jpg")
 label_img = tk.Label(frameRight, image = img_screen)
 label_img.pack()
-
-
-
-
 th2 = threading.Thread(target=scanGameAdb)
 th2.setDaemon(True)
 th2.start()
-
-
 window.mainloop()    

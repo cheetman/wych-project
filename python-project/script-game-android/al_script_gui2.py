dcc06c80d090db70123e0fb705443153c59d75cf
hpx <774022063@qq.com>
2020-10-28 10:34:24 +0800

    update

--- python-project/script-game-android/al_script_gui2.py
+++ python-project/script-game-android/al_script_gui2.py
@@ -30,30 +30,25 @@ def showSiftMatchImage(label_img,template,img_target_gray,kps_target,good,matche
 
 #创建sift检测器
 sift=cv.xfeatures2d.SIFT_create()
-templates = []
-templates.append(scriptModel.Template(sift, 1,"3.大获全胜S.jpg"))
-templates.append(scriptModel.Template(sift, 2,"3.大获全胜S2.jpg"))
-templates.append(scriptModel.Template(sift, 3,"3.战术碾压A.jpg"))
-templates.append(scriptModel.Template(sift, 4,"4.获得道具.jpg"))
-templates.append(scriptModel.Template(sift, 5,"6.大获全胜2确定按钮.jpg"))
-templates.append(scriptModel.Template(sift, 6,"5.大获全胜2.jpg"))
-templates.append(scriptModel.Template(sift, 7,"2.出击图标.jpg"))
-templates.append(scriptModel.Template(sift, 8,"1.立刻前往图标.jpg"))
 
+  
 
 template2 = scriptModel.Template(sift, 8,"8.地图条件.jpg")
-templates2 = []
-# templates2.append(scriptModel.Template(sift, 1,"8.敌人1.jpg"))
-# templates2.append(scriptModel.Template(sift, 1,"8.敌人2.jpg"))
-# templates2.append(scriptModel.Template(sift, 1,"8.敌人3.jpg"))
-# templates2.append(scriptModel.Template(sift, 1,"8.敌人4.jpg"))
-# templates2.append(scriptModel.Template(sift, 1,"8.敌人5.jpg"))
-# templates2.append(scriptModel.Template(sift, 1,"8.敌人6.jpg"))
-# templates2.append(scriptModel.Template(sift, 1,"8.敌人7.jpg"))
 
-files = os.listdir('.\8.敌人目录')
+
+
+templates = []
+templatesDir = '.\\1.图标点击'
+files = os.listdir(templatesDir)
 for file in files:
-  path = os.path.join('.\8.敌人目录', file)
+  path = os.path.join(templatesDir, file)
+  templates.append(scriptModel.Template(sift, 1,path))
+
+templates2 = []
+templatesDir = '.\\8.敌人目录'
+files = os.listdir(templatesDir)
+for file in files:
+  path = os.path.join(templatesDir, file)
   templates2.append(scriptModel.Template(sift, 1,path))
   
 
@@ -96,15 +91,15 @@ def task():
     img_rgb_hist = cv.calcHist([img_target_rgb],[0],None,[256],[0,256])
     cv.normalize(img_rgb_hist, img_rgb_hist,0,255*0.9,cv.NORM_MINMAX)
 
-    # 判断是否在战斗界面
+    
     success,good,matchesMask,dst = template2.matchSift(flann,img_target_gray,features,kps_target)
-    if success:
-      # 1.在战斗界面
-
-      # 1.1特殊地图
+    if success: # 判断是否在战斗界面
+    # 1.在战斗界面
       if checkBox5Value.get() == 1:
-        # 优先boss
-        if checkBox4Value.get() == 1:
+      # 1.1 特殊地图
+
+        
+        if checkBox4Value.get() == 1: # 判断是否优先Boss
           success,good,matchesMask,dst = templateBoss.matchSift(flann,img_target_gray,features,kps_target)
           if success:
             cv.polylines(img_target_gray,[np.int32(dst)],True,0,2, cv.LINE_AA)
@@ -112,7 +107,6 @@ def task():
             showSiftMatchImage(label_img,templateBoss,img_target_gray,kps_target,good,matchesMask)
             return
 
-        # 普通敌人
         for template in templates2:
           time.sleep(0.01)
           success,good,matchesMask,dst = template.matchSift(flann,img_target_gray,features,kps_target)
@@ -121,9 +115,9 @@ def task():
             subprocess.getstatusoutput([entry1Value.get(),"-s",entry4Value.get(),"shell","input","tap",str((dst[0][0][0] + dst[2][0][0])//2) ,str((dst[0][0][1] + dst[2][0][1])//2)])
             showSiftMatchImage(label_img,template,img_target_gray,kps_target,good,matchesMask)
       else:
+      # 1.2 普通地图
 
-        # 优先boss
-        if checkBox4Value.get() == 1:
+        if checkBox4Value.get() == 1: # 判断是否优先Boss
           success,good,matchesMask,dst = templateBoss.matchSift(flann,img_target_gray,features,kps_target)
           if success:
             cv.polylines(img_target_gray,[np.int32(dst)],True,0,2, cv.LINE_AA)
@@ -131,7 +125,6 @@ def task():
             showSiftMatchImage(label_img,templateBoss,img_target_gray,kps_target,good,matchesMask)
             return
 
-        # 普通敌人
         for template in templates2:
           time.sleep(0.01)
           success,good,matchesMask,dst = template.matchSift(flann,img_target_gray,features,kps_target)
@@ -141,10 +134,10 @@ def task():
             showSiftMatchImage(label_img,template,img_target_gray,kps_target,good,matchesMask)
             return
     
-    
     else:
-      # 2.不在战斗界面
-      if checkBox3Value.get() == 1:
+    # 2.不在战斗界面
+
+      if checkBox3Value.get() == 1: # 2.1 任务主界面
         for template in templates3:
           success = template.matchHist(img_rgb_hist)
           if success:
@@ -156,8 +149,8 @@ def task():
           if success:
             subprocess.getstatusoutput([entry1Value.get(),"-s",entry4Value.get(),"shell","input","tap", entry3Value.get().split(',')[0] ,entry3Value.get().split(',')[1]])
 
-      # 特别演习
-      if checkBox6Value.get() == 1:
+      
+      if checkBox6Value.get() == 1: # 2.2 特别演习
         for template in templates4:
           success = template.matchHist(img_rgb_hist)
           if success:
@@ -173,7 +166,7 @@ def task():
           if success:
             os.system( entry1Value.get() + " shell input tap "+ str(847) +" " + str(389))
 
-      for template in templates:
+      for template in templates: # 2.3 看见就点图标(例如任务成功)
         time.sleep(0.01)
         success,good,matchesMask,dst = template.matchSift(flann,img_target_gray,features,kps_target)
         if success:

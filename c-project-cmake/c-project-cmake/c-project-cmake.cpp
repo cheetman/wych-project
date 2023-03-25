c15a07d4b35cea78b56603a72e2ebe2c450dce39
cheetman <cheetman2@gmail.com>
2023-03-26 01:16:09 +0800

    update

--- c-project-cmake/c-project-cmake/c-project-cmake.cpp
+++ c-project-cmake/c-project-cmake/c-project-cmake.cpp
@@ -3,10 +3,294 @@
 
 #include "c-project-cmake.h"
 
+
 using namespace std;
 
-int main()
+
+
+void VulkanExample::handleMessages(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 {
-	cout << "Hello CMake." << endl;
+	switch (uMsg)
+	{
+	case WM_CLOSE:
+		prepared = false;
+		DestroyWindow(hWnd);
+		PostQuitMessage(0);
+		break;
+	case WM_PAINT:
+		ValidateRect(window, NULL);
+		break;
+	case WM_KEYDOWN:
+		switch (wParam)
+		{
+		case KEY_P:
+			paused = !paused;
+			break;
+		/*case KEY_F1:
+			UIOverlay.visible = !UIOverlay.visible;
+			UIOverlay.updated = true;
+			break;*/
+		case KEY_ESCAPE:
+			PostQuitMessage(0);
+			break;
+		}
+
+	/*	if (camera.type == Camera::firstperson)
+		{
+			switch (wParam)
+			{
+			case KEY_W:
+				camera.keys.up = true;
+				break;
+			case KEY_S:
+				camera.keys.down = true;
+				break;
+			case KEY_A:
+				camera.keys.left = true;
+				break;
+			case KEY_D:
+				camera.keys.right = true;
+				break;
+			}
+		}
+
+		keyPressed((uint32_t)wParam);*/
+		break;
+	case WM_KEYUP:
+		/*if (camera.type == Camera::firstperson)
+		{
+			switch (wParam)
+			{
+			case KEY_W:
+				camera.keys.up = false;
+				break;
+			case KEY_S:
+				camera.keys.down = false;
+				break;
+			case KEY_A:
+				camera.keys.left = false;
+				break;
+			case KEY_D:
+				camera.keys.right = false;
+				break;
+			}
+		}*/
+		break;
+	/*case WM_LBUTTONDOWN:
+		mousePos = glm::vec2((float)LOWORD(lParam), (float)HIWORD(lParam));
+		mouseButtons.left = true;
+		break;
+	case WM_RBUTTONDOWN:
+		mousePos = glm::vec2((float)LOWORD(lParam), (float)HIWORD(lParam));
+		mouseButtons.right = true;
+		break;
+	case WM_MBUTTONDOWN:
+		mousePos = glm::vec2((float)LOWORD(lParam), (float)HIWORD(lParam));
+		mouseButtons.middle = true;
+		break;
+	case WM_LBUTTONUP:
+		mouseButtons.left = false;
+		break;
+	case WM_RBUTTONUP:
+		mouseButtons.right = false;
+		break;
+	case WM_MBUTTONUP:
+		mouseButtons.middle = false;
+		break;*/
+	//case WM_MOUSEWHEEL:
+	//{
+	//	short wheelDelta = GET_WHEEL_DELTA_WPARAM(wParam);
+	//	camera.translate(glm::vec3(0.0f, 0.0f, (float)wheelDelta * 0.005f));
+	//	viewUpdated = true;
+	//	break;
+	//}
+	//case WM_MOUSEMOVE:
+	//{
+	//	handleMouseMove(LOWORD(lParam), HIWORD(lParam));
+	//	break;
+	//}
+	case WM_SIZE:
+		if ((prepared) && (wParam != SIZE_MINIMIZED))
+		{
+			if ((resizing) || ((wParam == SIZE_MAXIMIZED) || (wParam == SIZE_RESTORED)))
+			{
+			/*	destWidth = LOWORD(lParam);
+				destHeight = HIWORD(lParam);
+				windowResize();*/
+			}
+		}
+		break;
+	case WM_GETMINMAXINFO:
+	{
+		LPMINMAXINFO minMaxInfo = (LPMINMAXINFO)lParam;
+		minMaxInfo->ptMinTrackSize.x = 64;
+		minMaxInfo->ptMinTrackSize.y = 64;
+		break;
+	}
+	case WM_ENTERSIZEMOVE:
+		resizing = true;
+		break;
+	case WM_EXITSIZEMOVE:
+		resizing = false;
+		break;
+	}
+
+	//OnHandleMessage(hWnd, uMsg, wParam, lParam);
+}
+
+std::string VulkanExample::getWindowTitle()
+{
+	std::string device(deviceProperties.deviceName);
+	std::string windowTitle;
+	windowTitle = title + " - " + device;
+	if (!settings.overlay) {
+		windowTitle += " - " + std::to_string(frameCounter) + " fps";
+	}
+	return windowTitle;
+}
+
+
+HWND VulkanExample::setupWindow(HINSTANCE hinstance, WNDPROC wndproc) {
+	WNDCLASSEX wndClass;
+
+	wndClass.cbSize = sizeof(WNDCLASSEX);
+	wndClass.style = CS_HREDRAW | CS_VREDRAW;
+	wndClass.lpfnWndProc = wndproc;
+	wndClass.cbClsExtra = 0;
+	wndClass.cbWndExtra = 0;
+	wndClass.hInstance = hinstance;
+	wndClass.hIcon = LoadIcon(NULL, IDI_APPLICATION);
+	wndClass.hCursor = LoadCursor(NULL, IDC_ARROW);
+	wndClass.hbrBackground = (HBRUSH)GetStockObject(BLACK_BRUSH);
+	wndClass.lpszMenuName = NULL;
+	wndClass.lpszClassName = name.c_str();
+	wndClass.hIconSm = LoadIcon(NULL, IDI_WINLOGO);
+
+
+	if (!RegisterClassEx(&wndClass))
+	{
+		std::cout << "Could not register window class!\n";
+		fflush(stdout);
+		exit(1);
+	}
+
+	int screenWidth = GetSystemMetrics(SM_CXSCREEN);
+	int screenHeight = GetSystemMetrics(SM_CYSCREEN);
+
+	DWORD dwExStyle;
+	DWORD dwStyle;
+
+	if (settings.fullscreen)
+	{
+		dwExStyle = WS_EX_APPWINDOW;
+		dwStyle = WS_POPUP | WS_CLIPSIBLINGS | WS_CLIPCHILDREN;
+	}
+	else
+	{
+		dwExStyle = WS_EX_APPWINDOW | WS_EX_WINDOWEDGE;
+		dwStyle = WS_OVERLAPPEDWINDOW | WS_CLIPSIBLINGS | WS_CLIPCHILDREN;
+	}
+
+
+	RECT windowRect;
+	windowRect.left = 0L;
+	windowRect.top = 0L;
+	windowRect.right = settings.fullscreen ? (long)screenWidth : (long)width;
+	windowRect.bottom = settings.fullscreen ? (long)screenHeight : (long)height;
+
+
+	std::string windowTitle = getWindowTitle();
+	window = CreateWindowEx(0,
+		name.c_str(),
+		windowTitle.c_str(),
+		dwStyle | WS_CLIPSIBLINGS | WS_CLIPCHILDREN,
+		0,
+		0,
+		windowRect.right - windowRect.left,
+		windowRect.bottom - windowRect.top,
+		NULL,
+		NULL,
+		hinstance,
+		NULL);
+
+
+
+	ShowWindow(window, SW_SHOW);
+	SetForegroundWindow(window);
+	SetFocus(window);
+
+	return window;
+}
+
+void VulkanExample::renderLoop() {
+
+#if defined(_WIN32)
+	MSG msg;
+	bool quitMessageReceived = false;
+	while (!quitMessageReceived) {
+		while (PeekMessage(&msg, NULL, 0, 0, PM_REMOVE)) {
+			TranslateMessage(&msg);
+			DispatchMessage(&msg);
+			if (msg.message == WM_QUIT) {
+				quitMessageReceived = true;
+				break;
+			}
+		}
+		if (prepared && !IsIconic(window)) {
+			//nextFrame();
+		}
+	}
+#endif
+
+
+}
+
+
+void ConsoleAlloc() {
+#ifndef DISABLE_LOGGING_CONSOLE
+	AllocConsole();
+
+	SetConsoleTitleA("Debug Console");
+
+	freopen_s(reinterpret_cast<FILE**>(stdin), "conin$", "r", stdin);
+	freopen_s(reinterpret_cast<FILE**>(stdout), "conout$", "w", stdout);
+
+	::ShowWindow(GetConsoleWindow(), SW_SHOW);
+#endif
+}
+
+
+void ConsoleFree() {
+#ifndef DISABLE_LOGGING_CONSOLE
+	fclose(stdin);
+	fclose(stdout);
+
+
+	FreeConsole();
+
+#endif
+}
+
+VulkanExample* vulkanExample;
+LRESULT CALLBACK WndProc(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
+{
+	if (vulkanExample != NULL)
+	{
+		vulkanExample->handleMessages(hWnd, uMsg, wParam, lParam);
+	}
+	return (DefWindowProc(hWnd, uMsg, wParam, lParam));
+}
+
+
+int APIENTRY WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR pCmdLine, int nCmdShow)
+{
+	//for (size_t i = 0; i < __argc; i++) { VulkanExample::args.push_back(__argv[i]); };
+	ConsoleAlloc();
+	vulkanExample = new VulkanExample();
+	//vulkanExample->initVulkan();
+	vulkanExample->setupWindow(hInstance, WndProc);
+	//vulkanExample->prepare();
+	vulkanExample->renderLoop();
+	delete(vulkanExample);
 	return 0;
 }
--- c-project-cmake/c-project-cmake/c-project-cmake.h
+++ c-project-cmake/c-project-cmake/c-project-cmake.h
@@ -3,6 +3,116 @@
 
 #pragma once
 
+#if defined(_WIN32)
+#define KEY_ESCAPE VK_ESCAPE 
+#define KEY_F1 VK_F1
+#define KEY_F2 VK_F2
+#define KEY_F3 VK_F3
+#define KEY_F4 VK_F4
+#define KEY_F5 VK_F5
+#define KEY_W 0x57
+#define KEY_A 0x41
+#define KEY_S 0x53
+#define KEY_D 0x44
+#define KEY_P 0x50
+#define KEY_SPACE 0x20
+#define KEY_KPADD 0x6B
+#define KEY_KPSUB 0x6D
+#define KEY_B 0x42
+#define KEY_F 0x46
+#define KEY_L 0x4C
+#define KEY_N 0x4E
+#define KEY_O 0x4F
+#define KEY_T 0x54
+#endif
+
+
+
 #include <iostream>
+
+#pragma comment(linker, "/subsystem:windows")
+#include <windows.h>
+#include <fcntl.h>
+#include <io.h>
+#include <ShellScalingAPI.h>
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <assert.h>
+#include <vector>
+#include <array>
+#include <unordered_map>
+#include <numeric>
+#include <ctime>
+#include <iostream>
+#include <chrono>
+#include <random>
+#include <algorithm>
+#include <sys/stat.h>
+
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/matrix_inverse.hpp>
+#include <glm/gtc/type_ptr.hpp>
+#include <string>
+#include <numeric>
+#include <array>
+
+#include "vulkan/vulkan.h"
+
+class VulkanExample
+{
+
+public:
+	// OS specific
+#if defined(_WIN32)
+	HWND window;
+	HINSTANCE windowInstance;
+
+#endif
+
+	std::string title = "Vulkan Example";
+	std::string name = "vulkanExample";
+	uint32_t width = 1280;
+	uint32_t height = 720;
+
+
+	bool prepared = false;
+	bool resizing = false;
+	bool paused = false;
+	uint32_t frameCounter = 0;
+
+
+	struct Settings {
+		/** @brief Activates validation layers (and message output) when set to true */
+		bool validation = false;
+		/** @brief Set to true if fullscreen mode has been requested via command line */
+		bool fullscreen = false;
+		/** @brief Set to true if v-sync will be forced for the swapchain */
+		bool vsync = false;
+		/** @brief Enable UI overlay */
+		bool overlay = true;
+	} settings;
+
+
+	// Stores physical device properties (for e.g. checking device limits)
+	VkPhysicalDeviceProperties deviceProperties;
+
+
+
+
+
+
+
+
+	HWND setupWindow(HINSTANCE hinstance, WNDPROC wndproc);
+
+	void renderLoop();
+	std::string getWindowTitle();
+	void handleMessages(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
+};
+
+
 
 // TODO: 在此处引用程序需要的其他标头。
